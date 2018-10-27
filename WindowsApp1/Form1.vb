607aff88cffd36a03561bf5b4ef61ef032607c3a
maria <mariaavanza001@gmail.com>
2018-10-28 00:51:26 +0200

    commit0

---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Dim vector(10) As persona
+    Dim cont As Integer = 0
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim p As New persona(TextBox1.Text, Val(TextBox2.Text))
+        vector(cont) = p
+        cont = cont + 1
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim nom As String
+        Dim j As Integer = cont - 1
+        For i As Integer = 0 To j
+            nom = vector(i).setnom
+            MsgBox(nom)
+        Next
+        End
+        'finalizamos
+    End Sub
+End Class
---
+++ WindowsApp1/persona.vb
@@ -0,0 +1,21 @@
+﻿Public Class persona
+    Private nombre As String
+    Private edad As Integer
+
+    Public Sub New(nombre As String, edad As Integer)
+        Me.nombre = nombre
+        Me.edad = edad
+    End Sub
+    Public Sub getnombre(nom As String)
+        Me.nombre = nom
+    End Sub
+    Public Sub getedad(ed As Integer)
+        Me.nombre = ed
+    End Sub
+    Function setnom() As String
+        Return Me.nombre
+    End Function
+    Function setedad() As Integer
+        Return Me.edad
+    End Function
+End Class
